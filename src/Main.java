5d5319622f3800b3bd2c0dfa9794cd43fb3579c7
prabhjotg92 <prabhjotg@live.wsd1.org>
2026-03-31 23:58:48 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,65 @@
-// Simulation
 public class Main {
     public static void main(String[] args) {
-
-        Library lib = new Library();
+        Library myLibrary = new Library();
 
         while (true) {
+            myLibrary.currentDay = myLibrary.currentDay + 1;
+            System.out.println("\n--- Day " + myLibrary.currentDay + " ---");
 
-            // Advance time
-            lib.currentDay = lib.currentDay + 1;
-            System.out.println("\n--- Day " + lib.currentDay + " ---");
+            // Pick a random event
+            int eventChoice = Rand.randomInt(0, 4);
 
-            // simulate a random event
-            int randomNumber = Rand.randomInt(0, 4); // could generate 0, 1, 2, or 3
+            switch (eventChoice) {
+                case 0:
+                    System.out.println("It was a slow day at the library.");
+                    break;
 
-            switch (randomNumber) {
+                case 1:
+                    // Logic for borrowing a book
+                    int memberPos = Rand.randomInt(0, myLibrary.memberList.length);
+                    int bookPos = Rand.randomInt(0, myLibrary.bookCollection.length);
+                    
+                    Member person = myLibrary.memberList[memberPos];
+                    Book book = myLibrary.bookCollection[bookPos];
 
+                    if (book.isAvailable) {
+                        book.isAvailable = false;
+                        person.booksBorrowedCount++;
+                        myLibrary.activeLoans.add(new Loan(book, person, myLibrary.currentDay));
+                        System.out.println(person.name + " checked out " + book.title);
+                    } else {
+                        System.out.println(person.name + " wanted " + book.title + " but it's out.");
+                    }
+                    break;
 
+                case 2:
+                    // Logic for a random fine
+                    int fineIndex = Rand.randomInt(0, myLibrary.memberList.length);
+                    Member penalized = myLibrary.memberList[fineIndex];
+                    penalized.finesOwing += 2;
+                    System.out.println(penalized.name + " was late returning a book. $2 fine.");
+                    break;
+
+                case 3:
+                    System.out.println("A librarian organized the shelves.");
+                    break;
             }
 
-            Input.waitForUserToPressEnter("Press Enter to simulate the next day.");
+            // Display status of all books
+            System.out.println("\n--- Library Inventory ---");
+            for (int i = 0; i < myLibrary.bookCollection.length; i++) {
+                String shelfStatus = myLibrary.bookCollection[i].isAvailable ? "[In]" : "[Out]";
+                System.out.println(shelfStatus + " " + myLibrary.bookCollection[i].title);
+            }
+
+            // Display member stats
+            System.out.println("\n--- Member Records ---");
+            for (int i = 0; i < myLibrary.memberList.length; i++) {
+                Member m = myLibrary.memberList[i];
+                System.out.println(m.name + " | Total Borrowed: " + m.booksBorrowedCount + " | Fines: $" + m.finesOwing);
+            }
+
+            Input.waitForUserToPressEnter("\nPress Enter for Day " + (myLibrary.currentDay + 1));
         }
     }
 }
