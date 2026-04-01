5013cf9ca023f4c6b7c2bc3890320d98253356ee
prabhjotg92 <prabhjotg@live.wsd1.org>
2026-03-31 22:33:01 -0600

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,5 +1,12 @@
 public class Book {
-    public static void main(String[] args) {
+    String title;
+    String author;
+    boolean isAvailable;
 
+    // Constructor to set up the book
+    Book(String bookTitle, String bookAuthor) {
+        title = bookTitle;
+        author = bookAuthor;
+        isAvailable = true; // Books start on the shelf
     }
 }
