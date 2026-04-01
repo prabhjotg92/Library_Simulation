5782649ebacef4dea1dacb31cddb42be1bac9967
prabhjotg92 <prabhjotg@live.wsd1.org>
2026-03-31 23:52:59 -0600

    Update Member.java

--- src/Member.java
+++ src/Member.java
@@ -1,9 +1,11 @@
-public class Member {
+class Member {
     String name;
     int finesOwing;
+    int booksBorrowedCount; // Track how many books this person has taken total
 
     Member(String memberName) {
         name = memberName;
         finesOwing = 0;
+        booksBorrowedCount = 0;
     }
 }
