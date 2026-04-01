a25f20525f6aea8b72332d959cb6b8538e09bc2f
prabhjotg92 <prabhjotg@live.wsd1.org>
2026-03-31 23:54:02 -0600

    Update Loan.java

--- src/Loan.java
+++ src/Loan.java
@@ -2,10 +2,12 @@ public class Loan {
     Book borrowedBook;
     Member borrower;
     int dayBorrowed;
+    int dueDate; // New field to show when it should be back
 
-    Loan(Book b, Member m, int day) {
-        borrowedBook = b;
-        borrower = m;
+    Loan(Book theBook, Member theMember, int day) {
+        borrowedBook = theBook;
+        borrower = theMember;
         dayBorrowed = day;
+        dueDate = day + 7; // Books are due in 7 days
     }
 }
