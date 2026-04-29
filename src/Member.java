a08dcc344ccac7a589440664048e56b0ccba63c0
prabhjotg92 <prabhjotg@live.wsd1.org>
2026-04-29 01:08:47 -0500

    Update Member.java

--- src/Member.java
+++ src/Member.java
@@ -1,11 +1,37 @@
 public class Member {
-    String name;
-    int totalFines;
-     int totalBooksBorrowed; // Track how many books this person has taken total
+    private String name;
+    private int totalFines;
+    private int totalBooksBorrowed; 
 
     Member(String memberName) {
         name = memberName;
         totalFines = 0;
         totalBooksBorrowed = 0;
+    }
+
+    // Getters to see member info
+    public String getName() {
+        return name;
+    }
+
+    public int getTotalFines() {
+        return totalFines;
+    }
+
+    public int getTotalBooksBorrowed() {
+        return totalBooksBorrowed;
+    }
+
+    // Methods to update member stats
+    public void addFine(int amount) {
+        totalFines += amount;
+    }
+
+    public void payFines() {
+        totalFines = 0;
+    }
+
+    public void incrementBooksBorrowed() {
+        totalBooksBorrowed++;
     }
 }
